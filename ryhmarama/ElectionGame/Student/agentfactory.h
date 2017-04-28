a513b8dc2e10f2916a1cd7f44d8ba60721f4cec0
Lauri Solja <lauri.solja@student.tut.fi>
2017-04-28 23:03:15 +0300

    Dokumentaatio ja sopimussuunnittelu

--- ryhmarama/ElectionGame/Student/agentfactory.h
+++ ryhmarama/ElectionGame/Student/agentfactory.h
@@ -6,14 +6,41 @@
 using std::make_shared;
 using std::shared_ptr;
 
+/**
+ * @file
+ * @brief Agenttitehdasta kuvaava luokka
+ */
+
 namespace Interface
 {
+
+/**
+ * @brief AgentFactory on singleton, joka tuottaa aina eri nimisen agentin
+ *
+ * Jos luokan metodi ei ole poikkeustakuultaan nothrow, voi metodista aina vuotaa ulos poikkeus `std::bad_alloc` (muisti loppui).
+ */
     class AgentFactory
     {
         public:
+
+            /**
+             * @brief AgentFactory singleton-olio
+             */
             static AgentFactory AGENTFACTORY;
-            shared_ptr<Agent> createAgent();
+
+            /**
+             * @brief purkaja
+             */
             ~AgentFactory() = default;
+
+            /**
+             * @brief createAgent tuottaa uuden agentin uniikilla nimellä
+             * @pre -
+             * @return osoitin luotuun agenttiin
+             * @post Poikkeustakuu: vahva
+             */
+            shared_ptr<Agent> createAgent();
+
 
         private:
             AgentFactory();
--- ryhmarama/ElectionGame/Student/electionresult.h
+++ ryhmarama/ElectionGame/Student/electionresult.h
@@ -6,17 +6,61 @@
 #include "game.h"
 #include "locationdata.h"
 
+/**
+* @file
+* @brief Määrittelee pelin loppumisen yhteydessä pisteiden laskua kuvaavan luokan
+*/
+
 using std::shared_ptr;
 using Interface::Game;
 
+/**
+ * @brief ElectionResult on luokka, joka laskee pelin pisteet ja voittajan
+ *
+ * Pelin päätyttyä voi laskea pelaajien pisteet kutsumalla tämän luokan instanssia.
+ *
+ * Jos luokan metodi ei ole poikkeustakuultaan nothrow, voi metodista aina vuotaa ulos poikkeus `std::bad_alloc` (muisti loppui).
+ */
 class ElectionResult
 {
 public:
+
+    /**
+     * @brief ElectionResult rakentaja
+     */
     ElectionResult() = default;
+
+    /**
+     * @brief calculateWinner laskee pelin lopputuloksen
+     * @pre Peli on päättynyt, mutta pisteet laskematta
+     * @param *game osoitin peliobjektiin
+     * @post Pelin pelaajien pisteet on laskettu
+     */
     void calculateWinner(shared_ptr<Game> *game);
+
+    /**
+     * @brief getStatRows antaa pelaajien pisteet helposti käsiteltävässä rivimuodossa
+     * @pre Peli on päättynyt ja pisteet laskettu
+     * @post Poikkeustakuu: nothrow
+     */
     QVector<Options::electionResultRow> getStatRows() const;
+    /**
+     * @brief isTie kertoo päättyikö peli tasan
+     * @pre Peli on päättynyt ja pisteet laskettu
+     * @post Poikkeustakuu: nothrow
+     */
     bool isTie() const;
+    /**
+     * @brief getWinner antaa voittajan nimen
+     * @pre Peli on päättynyt ja pisteet laskettu
+     * @post Poikkeustakuu: nothrow
+     */
     QString getWinner() const;
+    /**
+     * @brief getTopElectionWinAmount kertoo montako ääntä voittaja sai
+     * @pre Peli on päättynyt ja pisteet laskettu
+     * @post Poikkeustakuu: nothrow
+     */
     unsigned getTopElectionWinAmount() const;
 
 private:
